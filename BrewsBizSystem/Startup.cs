f1006588b98fe2f8357eb3feca5147c92d3f3997
Caseywalker <casey.walker118@gmail.com>
2021-12-09 18:53:09 -0600

    Adding repository for all tables

--- BrewsBizSystem/Startup.cs
+++ BrewsBizSystem/Startup.cs
@@ -26,6 +26,7 @@ namespace BrewsBizSystem
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddSingleton<IConfiguration>(Configuration);
 
       services.AddControllers();
       services.AddSwaggerGen(c =>
