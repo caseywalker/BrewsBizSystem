7503a873c9c276c2b46e67f11ab967a263015f66
Caseywalker <casey.walker118@gmail.com>
2021-12-10 20:34:50 -0600

    Adding methods and controllers for Products

---
+++ BrewsBizSystem/Models/NewProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrewsBizSystem.Models
+{
+  public class NewProduct
+  {
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public decimal ProductPrice { get; set; }
+  }
+}
