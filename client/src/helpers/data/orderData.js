ff26fef6e523d0e95db06a75578f9743f8707313
Caseywalker <casey.walker118@gmail.com>
2021-12-18 13:01:23 -0600

    Adding data for quotes and orders

---
+++ client/src/helpers/data/orderData.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys';
+
+const dbURL = firebaseConfig.databaseURL;
+
+const getAllOrders = () => new Promise((resolve, reject) => {
+  axios.get(`${dbURL}/api/orders`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+const getOrderByID = (orderID) => new Promise((resolve, reject) => {
+  axios.get(`${dbURL}/api/orders/getOrderByID/${orderID}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+export { getAllOrders, getOrderByID };
